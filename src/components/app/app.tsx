827e83d5451e5ee65760d193da199b6b3449af86
Igor <golovkovviggreserv@gmail.com>
2025-08-02 16:25:53 +0300

    add: Маршруты к компонентам Удаление заглушек

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,51 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal onClose={() => ''} title={'Номер'}>
+            <OrderInfo />
+          </Modal>
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal title='Ингредиент' onClose={() => ''}>
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/profile/orders/:number' element={''} />
+    </Routes>
   </div>
 );
 
